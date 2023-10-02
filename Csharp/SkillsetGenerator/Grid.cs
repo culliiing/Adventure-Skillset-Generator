006c1b0cf05bf785f359b92c3d067dd694f239cc
Chris <103900975+culliiing@users.noreply.github.com>
2023-10-02 17:20:18 +0200

    Initial commit

---
+++ Csharp/SkillsetGenerator/Grid.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillsetGenerator
+{
+    internal class Grid
+    {
+        Random random = new Random();
+
+        int[,] grid;
+
+        string empty = "_";
+        string dead = "X";
+
+        public Grid(int width, int height)
+        {
+            this.grid = new int[width, height];
+        }
+
+        public void PrintEmpty()
+        {
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+                    Console.Write(empty);
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ Csharp/SkillsetGenerator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillsetGenerator
+{
+    internal class Program
+    {
+        static Random random = new Random();
+        Grid grid;
+
+        static void Main(string[] args)
+        {
+            Grid grid = GetNewGrid();
+            grid.PrintEmpty();
+
+            Console.ReadKey();
+        }
+
+        public static Grid GetNewGrid()
+        {
+
+            int height = 5;
+            int width = 4;
+
+            return new Grid(width, height);
+        }
+    }
+}
