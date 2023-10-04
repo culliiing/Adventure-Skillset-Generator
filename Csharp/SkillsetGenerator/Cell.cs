2b9fe152ebb1b8382d46d07d575f4149364f0503
Chris <103900975+culliiing@users.noreply.github.com>
2023-10-04 16:19:22 +0200

    Create Cell.cs

---
+++ Csharp/SkillsetGenerator/Cell.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillsetGenerator
+{
+    internal class Cell
+    {
+        int x;
+        int y;
+        bool enabled;
+
+        public Cell(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            this.enabled = true;
+        }
+
+        public Cell(int x, int y, bool enabled) : this(x, y)
+        {
+            this.enabled = enabled;
+        }
+    }
+}
