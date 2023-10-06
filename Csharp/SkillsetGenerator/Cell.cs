191fb933428e3b2f116e8dfa3362c8f355296ab0
Chris <103900975+culliiing@users.noreply.github.com>
2023-10-06 03:04:08 +0200

    Defines shapes, adds tools for manual generation

--- Csharp/SkillsetGenerator/Cell.cs
+++ Csharp/SkillsetGenerator/Cell.cs
@@ -23,5 +23,17 @@ namespace SkillsetGenerator
         {
             this.enabled = enabled;
         }
+
+        public void Print()
+        {
+            Console.WriteLine($"({x},{y})");
+        }
+
+        public bool Overlaps(int x, int y)
+        {
+            if (this.x == x && this.y == y)
+                return true;
+            return false;
+        }
     }
 }
--- Csharp/SkillsetGenerator/Shape.cs
+++ Csharp/SkillsetGenerator/Shape.cs
@@ -8,6 +8,87 @@ namespace SkillsetGenerator
 {
     internal class Shape
     {
-        
+        string name;
+        char symbol;
+        List<Cell> cells;
+
+        /// <summary>
+        /// Build your own Shape with manual instructions.
+        /// </summary>
+        /// <param name="instructions">Input string of directions where 8=up, 6=right, 2=down, 4=left.</param>
+        public Shape(string name, string instructions)
+        {
+            this.name = name.ToUpper();
+            this.symbol = this.name[0];
+            this.cells = new List<Cell>() { new Cell(0,0) };
+            this.Build(instructions);
+        }
+
+        public Shape(string name, List<Cell> cells)
+        {
+            this.name = name.ToUpper();
+            this.symbol = this.name[0];
+            this.cells = cells;
+        }
+
+        /// <summary>
+        /// Adds Cells to a Shape in a tree-like or branching fashion based on inputted directions.
+        /// </summary>
+        /// <param name="instructions">Input string of directions where 8=up, 6=right, 2=down, 4=left.</param>
+        /// <param name="x">Enter the x coordinate from where the Shape will begin to expand. Must be from within the Shape.</param>
+        /// <param name="y">Enter the y coordinate from where the Shape will begin to expand. Must be from within the Shape.</param>
+        public void Build(string instructions, int x = 0, int y = 0)
+        {
+            if (!Contains(x, y))
+                throw new Exception("Cannot branch from non-existant Cell.");
+
+            for (int i = 0; i < instructions.Length; i++)
+            {
+                switch (instructions[i])
+                {
+                    case '6':
+                        y++;
+                        break;
+                    case '4':
+                        y--;
+                        break;
+                    case '8':
+                        x--;
+                        break;
+                    case '2':
+                        x++;
+                        break;
+                    default:
+                        break;
+                }
+
+                if(!Contains(x,y))
+                    cells.Add(new Cell(x,y));
+            }
+        }
+
+        /// <summary>
+        /// Checks whether a Cell with coordinates x and y (relative to the Shape's origin) exists within the Shape.
+        /// </summary>
+        public bool Contains(int x, int y)
+        {
+            foreach (Cell cell in cells)
+                if(cell.Overlaps(x,y))
+                    return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Prints the coordinates of each Cell in the shape, relative to the shape.
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine(this.name);
+            foreach (Cell cell in cells)
+            {
+                cell.Print();
+            }
+        }
     }
 }
