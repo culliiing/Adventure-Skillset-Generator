6581cdf7bc684971ecc9388926522e3de2b243d2
SalamSki <slome.lord123@gmail.com>
2023-10-09 22:07:49 +0200

    First version of simulated insertion of A and B

--- python/grid.py
+++ python/grid.py
@@ -8,13 +8,38 @@ class Grid:
     for row in matrix:
       print(('|'+'{}'*len(row)).format(*['{}|'.format(c) for c in row]))
   
+  def get_free(self):
+    return sorted([(idx%len(self.matrix),idx%len(self.matrix[0])) for idx in range(len(self.matrix) * len(self.matrix[0])) if self.matrix[idx%len(self.matrix)][idx%len(self.matrix[0])] == ' '])
+
   def add(self,shape, x, y):
     matrix = self.matrix
     name = shape.get_name()
-    for cell in shape.get():
-      shape_x, shape_y = cell
+    cells  = shape.get()
+    added = []
+    for i in range(len(cells)):
+      shape_x, shape_y = cells[i]
       temp_x = shape_x + x
       temp_y = shape_y + y
-      if  temp_x >= 0 and temp_x < len(matrix) and temp_y >= 0 and temp_y < len(matrix[0]):
+      if self._in_bounds(temp_x, temp_y):
         if matrix[temp_x][temp_y] == ' ':
           matrix[temp_x][temp_y] = name
+          added.append((temp_x,temp_y))
+        else: 
+          print(f'{name} taken at {(temp_x,temp_y)}')
+          self._clear(added)
+          return False
+      else:
+        print(f'{name} out_of_bounds at {(temp_x,temp_y)}')
+        self._clear(added)
+        return False
+    return True
+  
+  def _clear(self, cells):
+    matrix = self.matrix
+    for x,y in cells:
+      if self._in_bounds(x, y):
+        matrix[x][y] = ' '
+  
+  def _in_bounds(self,x,y):
+    return x >= 0 and x < len(self.matrix) and y >= 0 and y < len(self.matrix[0])
+      
--- python/main.py
+++ python/main.py
@@ -1,11 +1,27 @@
 from shape import Shape
 from grid import Grid
+import random as rand
 
-grid = Grid(5, 4)
+#Create grid with random dim 
+GRID_X,GRID_Y = rand.randint(4,4),rand.randint(3,3) 
+grid = Grid(GRID_X, GRID_Y)
 A = Shape('A', 'd r')
 B = Shape('B', 'l d')
-grid.add(A, 0, 0)
-grid.add(B, 2, 1)
+shapes = [A,B]
+
+shape_loop_counter = 0
+while shape_loop_counter < len(shapes):
+  shape = shapes[shape_loop_counter]
+  free_cells = grid.get_free()
+  
+  while len(free_cells) > 0:
+    idx = rand.randint(0, len(free_cells)-1) 
+    x,y = free_cells[idx]
+    if grid.add(shape,x,y): 
+      shape_loop_counter+=1
+      break
+    else:
+      del free_cells[idx]
 grid.out()
 
-print(A.get(), B.get(), A.compare(B))
+#print(A.get(), B.get(), A.compare(B))
