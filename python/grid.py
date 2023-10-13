69dd02775951982b3aafe77d0c0b524c2fe719c7
SalamSki <48717734+lordslomer@users.noreply.github.com>
2023-10-13 20:31:26 +0200

    recursion

--- python/grid.py
+++ python/grid.py
@@ -1,3 +1,5 @@
+import random as rand
+
 class Grid:
   
   def __init__(self, x,y):
@@ -10,6 +12,29 @@ class Grid:
   
   def get_free(self):
     return sorted([(idx%len(self.matrix),idx%len(self.matrix[0])) for idx in range(len(self.matrix) * len(self.matrix[0])) if self.matrix[idx%len(self.matrix)][idx%len(self.matrix[0])] == ' '])
+
+  def add_shapes(self, shapes):
+    num = len(shapes)
+    if num > 0:
+      shape = shapes[0]
+      if num == 1:    
+        return self.try_cells(shape, self.get_free())
+      
+      return self.try_cells(shape, self.get_free()) and self.add_shapes(shapes[1:])
+    else:
+      raise Exception('Length of shapes must be bigger than 0.')
+
+  def try_cells(self, shape, cells):
+    free_cells = cells
+    while len(free_cells) > 0:
+      idx = rand.randint(0, len(free_cells)-1)
+      x, y = free_cells[idx]
+      added = self.add(shape, x, y)
+      if added:
+        return True
+      else:
+        del free_cells[idx]
+    return False
 
   def add(self,shape, x, y):
     matrix = self.matrix
--- python/main.py
+++ python/main.py
@@ -1,27 +1,23 @@
 from shape import Shape
 from grid import Grid
 import random as rand
+from time import sleep
+from os import system
 
-#Create grid with random dim 
-GRID_X,GRID_Y = rand.randint(4,4),rand.randint(3,3) 
+system('cls')
+print('Output:\n')
+
+# Create grid with random dim
+GRID_X, GRID_Y = rand.randint(3, 3), rand.randint(3, 3)
 grid = Grid(GRID_X, GRID_Y)
+
+#Create two shapes A and B
 A = Shape('A', 'd r')
 B = Shape('B', 'l d')
-shapes = [A,B]
+shapes = [A, B]
 
-shape_loop_counter = 0
-while shape_loop_counter < len(shapes):
-  shape = shapes[shape_loop_counter]
-  free_cells = grid.get_free()
-  
-  while len(free_cells) > 0:
-    idx = rand.randint(0, len(free_cells)-1) 
-    x,y = free_cells[idx]
-    if grid.add(shape,x,y): 
-      shape_loop_counter+=1
-      break
-    else:
-      del free_cells[idx]
+print(grid.add_shapes(shapes))
+
 grid.out()
 
 #print(A.get(), B.get(), A.compare(B))
